e5ccac637cd7b060303b334ee40ae8325c4ad172
trant <trantai306@gmail.com>
2024-12-19 00:57:44 +0700

    # fix animation synchronous

--- Entities/AnimatedObject.cs
+++ Entities/AnimatedObject.cs
@@ -1,14 +1,63 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Drawing;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace Project_Game.Entities
+//namespace Project_Game.Entities
+//{
+//    public class AnimatedObject
+//    {
+//        private AnimationManager animationManager;
+//        private int x, y, width, height;
+
+//        public AnimatedObject(string spriteSheetFolderPath, int x, int y, int width, int height, int frameRate = 5)
+//        {
+//            this.x = x;
+//            this.y = y;
+//            this.width = width;
+//            this.height = height;
+
+//            animationManager = new AnimationManager(frameRate);
+//            animationManager.LoadFrames(spriteSheetFolderPath); // Tải các frame từ thư mục
+//        }
+
+//        public void Update()
+//        {
+//            animationManager.UpdateAnimation();  // Cập nhật hoạt ảnh
+//        }
+
+//        public void Draw(Graphics g)
+//        {
+//            var currentFrame = animationManager.GetCurrentFrame();
+//            if (currentFrame != null)
+//            {
+//                g.DrawImage(currentFrame, x, y, width, height);  // Vẽ frame
+//            }
+//        }
+
+//        public void ResetAnimation()
+//        {
+//            animationManager.ResetAnimation();
+//        }
+
+//        public bool IsComplete()
+//        {
+//            return animationManager.IsComplete();
+//        }
+//    }
+
+//}
+using Project_Game.Entities;
+using System.Drawing;
+using System;
+
+namespace Project_Game.Entities // Đảm bảo AnimatedObject thuộc về cùng namespace
 {
     public class AnimatedObject
     {
+        private static readonly Random rand = new Random(); // Nguồn Random chung
         private AnimationManager animationManager;
         private int x, y, width, height;
 
@@ -21,6 +70,19 @@ namespace Project_Game.Entities
 
             animationManager = new AnimationManager(frameRate);
             animationManager.LoadFrames(spriteSheetFolderPath); // Tải các frame từ thư mục
+
+            // Thiết lập frame bắt đầu ngẫu nhiên nếu có
+            if (animationManager.GetFrameCount() > 0)
+            {
+                int initialFrame = rand.Next(0, animationManager.GetFrameCount());
+                animationManager.SetCurrentFrame(initialFrame);
+                Console.WriteLine($"AnimatedObject: Khởi tạo với frame {initialFrame}");
+
+                // Thiết lập frameRateCounter ngẫu nhiên để phá đồng bộ hóa
+                int randomCounter = rand.Next(0, animationManager.FrameRateLimit);
+                animationManager.SetFrameRateCounter(randomCounter);
+                Console.WriteLine($"AnimatedObject: Khởi tạo với frameRateCounter {randomCounter}");
+            }
         }
 
         public void Update()
@@ -47,5 +109,5 @@ namespace Project_Game.Entities
             return animationManager.IsComplete();
         }
     }
-
 }
+
